2c2282af9aa2e8808453fdaf8568d7f2c7bbbfb9
Ramyaeaswaran <ramyaeaswaran77@gmail.com>
2019-10-01 13:32:36 +0530

    day2 layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,56 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(),
+  Container container1 = Container(
+    width: 100,
+    height: 100,
+    color: Colors.red,
+    //margin: EdgeInsets.symmetric(vertical: 20.0),
+  );
+  Container container2 = Container(
+    width: 100,
+    height: 100,
+    color: Colors.blue,
+  );
+
+  Container container3 =
+      Container(width: 100, height: 100, color: Colors.yellow);
+  Container container4 =
+      Container(width: 100, height: 100, color: Colors.green);
+  List<Widget> contain2 = [container3, container4];
+  List<Widget> contain1 = [
+    container1,
+    Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: contain2,
+    ),
+    container2
+  ];
+
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.purple,
+      body: SafeArea(
+        child: Container(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.red,
+                //margin: EdgeInsets.symmetric(vertical: 20.0),
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: contain2,
+              ),
+              container2
+            ],
+          ),
+        ),
       ),
     ),
-  );
+  ));
 }
